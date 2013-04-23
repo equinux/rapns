eb132ffeaac91f451b3e453bee9962df3fbe0cf2
Maximilian Szengel <szengel@equinux.com>
2013-04-23 16:53:32 +0200

    Fix SQLite3 definition

--- lib/rapns/daemon/database_reconnectable.rb
+++ lib/rapns/daemon/database_reconnectable.rb
@@ -1,11 +1,8 @@
 class PGError < StandardError; end if !defined?(PGError)
-if not defined?(SQLite3).nil?
+if not defined?(SQLite3)
   module SQLite3; class Exception < StandardError; end; end
 end
 class Mysql; class Error < StandardError; end; end if !defined?(Mysql)
-if not defined?(SQLite3).nil?
-  module SQLite3; class Exception < StandardError; end; end 
-end
 module Mysql2; class Error < StandardError; end; end if !defined?(Mysql2)
 module ActiveRecord; end
 class ActiveRecord::JDBCError < StandardError; end if !defined?(ActiveRecord::JDBCError)
